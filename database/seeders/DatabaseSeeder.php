9015dd1bd3ec9a24ff0a903f42386d68e66c6b0b
coderkhayrul <coderkhayrul@gmail.com>
2025-03-16 01:56:17 +0600

    Init Project

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Product;
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $categories = Category::factory(10)->create();
+        User::factory(5)
+            ->has(
+                Product::factory(10)->state(function () use ($categories) {
+                    return [
+                        'category_id' => $categories->random()
+                    ];
+                })
+            )->create();
+    }
+}
